6a267817ddbd17c2d332e43089cc72a1c528de30
Stephanie Truong <4.struong@kscholars.org>
2019-10-12 16:49:30 -0500

    added gifs but not the right endpoint, won't stop gif movement

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -27,5 +27,45 @@ $('#add-gif').on('click', function(event) {
     renderButtons();
 })
 
+$(document).on('click', function(){
+    var characterGif = $(this).attr('data-name');
+
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+    characterGif + "&api_key=J7dN5FuWri9WAcHYqLxKiKeR8JifGnwY&limit=10";
+
+    $.ajax({
+        url: queryURL,
+        method: 'GET'
+    }).then(function(response){
+        console.log(queryURL)
+        console.log(response)
+        var results = response.data;
+        for (var i = 0; i < results.length; i++){
+            if (results[i].rating !== 'r') {
+                var gifDiv = $('div');
+                var rating = results[i].rating;
+                var p = $('<p>').text('Rating: ' + rating);
+                var characterImg = $('<img>');
+                characterImg.attr('src', results[i].images.fixed_height.url);
+                gifDiv.append(p);
+                gifDiv.append(characterImg);
+    
+                // $('.gif-view').prepend(gifDiv)
+            }
+        }
+    })
+})
+
+$('.gif').on('click', function(){
+    var state = $(this).attr("data-state");
+    if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+      } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+      }
+})
+
 // Have initial buttons show 
 renderButtons();
